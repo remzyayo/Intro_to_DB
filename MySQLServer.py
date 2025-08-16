bb2142504057670d598b5008cbf8955b98765d0b
REMZY <oluremifrancisaya@gmail.com>
2025-08-16 07:44:14 -0700

    MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -16,7 +16,7 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
@@ -28,3 +28,4 @@ def create_database():
 
 if _name_ == "_main_":
     create_database()
+
